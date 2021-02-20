ac0133e9fd065bdc064caea3bf7d7d248a55a628
Krut Patel <kroot.patel@gmail.com>
2021-02-20 10:01:58 +0530

    LF Redirector: Loop until header element is available

--- leetfree-redirector.js
+++ leetfree-redirector.js
@@ -1,19 +1,34 @@
 // ==UserScript==
 // @name         Leetfree redirector
 // @namespace    iamkroot
-// @version      0.1
+// @version      0.2
 // @description  Redirect locked Leetcode questions to leetfree site
 // @author       iamkroot
 // @match        https://leetcode.com/problems/*
 // @grant        none
+// @run-at       document-idle
 // ==/UserScript==
 
 (function() {
+    const insertHeader = (url, headerEl) => {
+        let free_node = headerEl.cloneNode(true);
+        free_node.setAttribute('data-cy', 'leetfree');
+        free_node.setAttribute('data-key', 'leetfree');
+        let a_node = free_node.querySelector("a");
+        a_node.href = url;
+        a_node.querySelectorAll("span")[1].innerText = "LF";
+        headerEl.parentElement.appendChild(free_node);
+    }
     'use strict';
     let desc = document.querySelector("div[data-cy='description-content']");
-    if (!!desc && desc.firstChild.className.startswith("description")) {
-        return;
-    }
     const new_subpath = document.location.pathname.replace(/\/$/, ".html");
-    document.location = "https://heip.github.io/LeetCodePremium" + new_subpath;
+    const url = "https://heip.github.io/LeetCodePremium" + new_subpath;
+    const insertNew = () => {
+        const desc = document.querySelector("div[data-cy='description']");
+        if (desc == null) {
+            return setTimeout(insertNew, 1000);
+        }
+        insertHeader(url, desc);
+    }
+    insertNew();
 })();
