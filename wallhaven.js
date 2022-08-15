7825eb1a7bb6dcc8d8a57b0e96f353a103216cc4
iamkroot <kroot.patel@gmail.com>
2022-08-15 18:31:48 +0530

    wallhaven: Shortcuts for Add/Remove from favorites

--- wallhaven.js
+++ wallhaven.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name         Wallhaven Enhanced
 // @namespace    iamkroot
-// @version      0.1
+// @version      0.2
 // @description  Download and Fav the wallpaper; Press F to toggle Fullscreen
 // @author       iamkroot
 // @match        https://wallhaven.cc/w/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=wallhaven.cc
-// @grant        GM_registerMenuCommand
 // @grant        GM_download
 // ==/UserScript==
 
@@ -36,6 +35,17 @@
         return true;
     }
 
+    function removeFromFav() {
+        let elems = document.getElementsByClassName("rm-button in-favorites");
+        if (elems.length == 0) {
+            console.warn('Unable to find "Remove from Favorites" Button')
+            return false;
+        }
+        let elem = elems[0];
+        elem.click();
+        return true;
+    }
+
     function download() {
         let elem = document.getElementById("wallpaper");
         let url = elem.src;
@@ -55,8 +65,10 @@
 
     document.addEventListener('keydown', (event) => {
         switch (event.key) {
-            case 'f': toggleFullscreen(); break;
+            case 'a': addToFav(); break;
             case 'd': downloadAndFav(); break;
+            case 'f': toggleFullscreen(); break;
+            case 'r': removeFromFav(); break;
         }
     }, false);
 })();
